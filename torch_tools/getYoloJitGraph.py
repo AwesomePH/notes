e7fd6ba5ea62643c223efe089e2b6f47355f7dca
sdx <shidongxing@foxmail.com>
2020-10-16 16:56:10 +0800

    add jit.script demo

--- torch_tools/getYoloJitGraph.py
+++ torch_tools/getYoloJitGraph.py
@@ -7,3 +7,12 @@ def getgraph(cfgfile):
   traced = torch.jit.trace(m, input, check_trace=False)
   graph,w = torch._C._jit_pass_lower_graph(traced.graph, traced._c)
   print(graph)
+
+def getJitScript(model):
+    script = torch.jit.script(model)
+    print(script.graph)
+    # names = torch.jit.export_opnames(script)
+    # for n in names:
+    #     s.add(n)
+    # for i in s:
+    #     print(i)
